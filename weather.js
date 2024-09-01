6633d2b32376191f2ce5fded14caebe954bfdf81
shalini322 <155485757+shalini322@users.noreply.github.com>
2024-09-01 17:09:33 +0530

    weather.js

---
+++ weather.js
@@ -0,0 +1,32 @@
+const apiKey = "312abfd953c970ceaf81f671079f6e22";
+const apiUrl = "https://api.openweathermap.org/data/2.5/weather?units=metric&q=";
+
+const searchBox = document.querySelector(".search input");
+const searchBtn = document.querySelector(".search button");
+const weatherIcon = document.querySelector(".weather-icon");
+
+async function checkWeather(city) {
+    const response = await fetch(`${apiUrl}${city}&appid=${apiKey}`);
+
+    if (response.status === 404) {
+        document.querySelector(".error").style.display = "block";
+        document.querySelector(".weather").style.display = "none";
+    } else {
+        const data = await response.json();
+
+        document.querySelector(".city").textContent = data.name;
+        document.querySelector(".temp").textContent = `${Math.round(data.main.temp)}°C`;
+        document.querySelector(".humidity").textContent = `${data.main.humidity}%`;
+        document.querySelector(".wind").textContent = `${data.wind.speed}km/h`;
+
+        const weatherCondition = data.weather[0].main.toLowerCase();
+        weatherIcon.src = `${weatherCondition}.png`;
+
+        document.querySelector(".weather").style.display = "block";
+        document.querySelector(".error").style.display = "none";
+    }
+}
+
+searchBtn.addEventListener("click", () => {
+    checkWeather(searchBox.value);
+});
